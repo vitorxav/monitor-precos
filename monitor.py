72b6f608b6bf5bee25248d63558a3da1ad8935ae
vitor <vitordias.xavier1@gmail.com>
2025-05-14 01:56:39 -0300

    Finalizando interface e removendo comentarios e testes unitarios

--- monitor.py
+++ monitor.py
@@ -3,14 +3,6 @@ from scraper.selenium_scraper import get_price_with_selenium
 from alert import enviar_alerta
 
 def monitorar_produto(url, css_selector, preco_alvo, intervalo=60):
-    """
-    Monitora o preço de um produto e avisa quando estiver abaixo do valor desejado.
-
-    url: link do produto
-    css_selector: seletor CSS do preço
-    preco_alvo: valor desejado (alvo)
-    intervalo: tempo (em segundos) entre cada checagem
-    """
     while True:
         print("[INFO] Verificando preço...")
 
@@ -29,10 +21,3 @@ def monitorar_produto(url, css_selector, preco_alvo, intervalo=60):
 
         print(f"[INFO] Aguardando {intervalo} segundos para nova verificação...\n")
         time.sleep(intervalo)
-
-#if __name__ == "__main__":
-#    url = "https://www.kabum.com.br/produto/205760/gravador-dvd-rw-sata-asus-24x-drw-24f1mt-blk-b-as"  # Substitua com seu link
-#    css_selector = "#blocoValores > div.sc-a24aba34-3.hSVqxN > div.sc-a24aba34-1.cpLDBn > div > h4"                # Substitua com seu seletor
-#    preco_alvo = 90.00                                # Alvo em R$
-
-#    monitorar_produto(url, css_selector, preco_alvo, intervalo=120)
